0fbae515c5467f3151f9cba1a79c5d210c92d825
Marc Holman <marcholman9323@gmail.com>
2019-04-18 18:40:27 -0500

    Updated application properties with some basic h2 & datasource

--- src/main/java/com/luv2code/springit/SpringitApplication.java
+++ src/main/java/com/luv2code/springit/SpringitApplication.java
@@ -1,16 +1,15 @@
 package com.luv2code.springit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
-
-;
 
 @SpringBootApplication
 public class SpringitApplication {
 
 
+    private static final Logger log = LoggerFactory.getLogger(SpringitApplication.class);
 
     public static void main(String[] args) {
 
